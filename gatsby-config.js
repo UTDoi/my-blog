8663853a46633017d087771c43e3868952c0eafe
UTDoi <doisaltu1899@gmail.com>
2021-11-16 01:31:50 +0900

    :sparkles: introduce tailwindcss + emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-typegen`,
+    `gatsby-plugin-emotion`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
