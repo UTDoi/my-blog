b32bd8f60fa1dbee70f33f16aafc8ae503e5b2f2
UTDoi <doisaltu1899@gmail.com>
2021-11-20 12:50:56 +0900

    :shower: remove unused image and add sample text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -10,14 +9,41 @@ const IndexPage: React.VFC = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
+    <p>hogehoge hogehoge</p>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </p>
