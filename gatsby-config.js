148873fec0b73c78079cd1986cda55d854b275b6
UTDoi <doisaltu1899@gmail.com>
2021-11-20 11:35:43 +0900

    :+1: update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: "UTLOG",
+    description: "UTDoi's tech blog powered by gatsby",
+    author: "@UTDoi",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
