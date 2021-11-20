b7605f1fd5910b6ba90dda1ac6ce19b9166e731d
UTDoi <doisaltu1899@gmail.com>
2021-11-20 23:57:54 +0900

    :+1: create base header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,16 @@
 import { Link } from "gatsby"
 import * as React from "react"
+import "twin.macro"
 
 type Props = {
   siteTitle: string
 }
 
 const Header: React.VFC<Props> = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header tw="text-white bg-black opacity-60 sticky top-0">
+    <h1 tw="text-2xl tracking-widest p-3 antialiased">
+      <Link to="/">{siteTitle}</Link>
+    </h1>
   </header>
 )
 
