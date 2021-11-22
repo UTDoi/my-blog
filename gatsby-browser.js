786720452af49dd5daad64b8773e01573e3358ed
UTDoi <doisaltu1899@gmail.com>
2021-11-22 14:50:25 +0900

    :sparkles: import font for light

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 import "@fontsource/noto-sans-jp"
+import "@fontsource/noto-sans-jp/300.css"
 import "@fontsource/noto-sans-jp/700.css"
 import "@fontsource/roboto"
+import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/700.css"
