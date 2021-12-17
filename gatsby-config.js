4caec5a017b684705622fab5fb835b1aa6e319fe
UTDoi <doisaltu1899@gmail.com>
2021-12-17 22:34:22 +0900

    :sparkles: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `content/assets/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typegen`,
