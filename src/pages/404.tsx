8d4c9a181cfd7ec37f1c506f0e98ccea8d123ee7
UTDoi <doisaltu1899@gmail.com>
2021-12-17 23:28:06 +0900

    :sparkles: modify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,18 @@
+import { Link } from "gatsby"
 import * as React from "react"
+import "twin.macro"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 
 const NotFoundPage: React.VFC = () => (
   <Layout>
     <Seo title="Not Found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div tw="mt-10 text-center">
+      <h1 tw="text-3xl font-bold p-3">404 Not Found</h1>
+      <Link to="/">
+        <span tw="border-b-black border-b">トップページへ</span>
+      </Link>
+    </div>
   </Layout>
 )
 
